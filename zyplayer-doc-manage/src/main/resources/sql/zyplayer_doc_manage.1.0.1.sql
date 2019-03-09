90ce37ec359f061ea1baafd938551b91d15e4575
暮光：城中城 <806783409@qq.com>
2019-03-09 23:32:36 +0800

    建库脚本

--- zyplayer-doc-manage/src/main/resources/sql/zyplayer_doc_manage.1.0.1.sql
+++ zyplayer-doc-manage/src/main/resources/sql/zyplayer_doc_manage.1.0.1.sql
@@ -11,7 +11,7 @@
  Target Server Version : 50725
  File Encoding         : 65001
 
- Date: 08/03/2019 22:38:59
+ Date: 09/03/2019 23:31:30
 */
 
 SET NAMES utf8mb4;
@@ -107,6 +107,7 @@ CREATE TABLE `wiki_page`  (
   `parent_id` bigint(20) NOT NULL DEFAULT 0 COMMENT '父ID',
   `node_type` tinyint(4) NULL DEFAULT 0 COMMENT '节点类型 0=有子节点 1=终节点',
   `zan_num` int(11) NOT NULL DEFAULT 0 COMMENT '赞的数量',
+  `edit_type` tinyint(4) NOT NULL DEFAULT 0 COMMENT '编辑类型 0=可编辑 1=不允许编辑',
   `create_user_id` bigint(20) NULL DEFAULT NULL COMMENT '创建人ID',
   `create_user_name` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '创建人名字',
   `create_time` datetime(0) NULL DEFAULT NULL COMMENT '创建时间',
@@ -115,20 +116,17 @@ CREATE TABLE `wiki_page`  (
   `update_time` datetime(0) NULL DEFAULT NULL COMMENT '修改时间',
   `del_flag` tinyint(4) NOT NULL DEFAULT 0 COMMENT '0=有效 1=删除',
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 19 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 7 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of wiki_page
 -- ----------------------------
-INSERT INTO `wiki_page` VALUES (7, 1, '测试1', 0, 0, 1, 1, NULL, '2019-02-24 17:51:56', 1, NULL, '2019-02-24 17:51:56', 0);
-INSERT INTO `wiki_page` VALUES (8, 1, '测试2', 0, 0, 4, 1, NULL, '2019-02-24 17:51:56', 1, '暮光：城中城', '2019-03-08 22:12:36', 0);
-INSERT INTO `wiki_page` VALUES (9, 1, '测试3', 7, 0, 0, 1, NULL, '2019-02-24 17:51:56', 1, '暮光：城中城', '2019-03-08 22:12:51', 0);
-INSERT INTO `wiki_page` VALUES (10, 1, '测试4', 7, 0, 0, 1, NULL, '2019-02-24 17:51:56', 1, NULL, '2019-02-24 17:51:56', 0);
-INSERT INTO `wiki_page` VALUES (11, 1, '测试5', 9, 0, 0, 1, NULL, '2019-02-24 17:51:56', 1, NULL, '2019-02-24 17:51:56', 0);
-INSERT INTO `wiki_page` VALUES (12, 1, '测试6', 7, 0, 0, 1, NULL, '2019-02-24 17:51:56', 1, NULL, '2019-02-24 17:51:56', 0);
-INSERT INTO `wiki_page` VALUES (14, 1, '测试7', 0, 0, 2, 1, NULL, '2019-02-24 17:51:56', 1, NULL, '2019-02-24 17:51:56', 0);
-INSERT INTO `wiki_page` VALUES (17, 2, '数据库团队介绍', 0, 0, 0, 1, '暮光：城中城', '2019-03-07 22:07:10', NULL, NULL, NULL, 0);
-INSERT INTO `wiki_page` VALUES (18, 2, '人员列表', 17, 0, 1, 1, '暮光：城中城', '2019-03-07 22:28:47', NULL, NULL, NULL, 0);
+INSERT INTO `wiki_page` VALUES (1, 1, '关于zyplayer-doc工具', 0, 0, 0, 0, 1, '暮光：城中城', '2019-03-09 14:01:41', 1, '暮光：城中城', '2019-03-09 15:30:22', 0);
+INSERT INTO `wiki_page` VALUES (2, 1, '开发规划', 0, 0, 0, 0, 1, '暮光：城中城', '2019-03-09 14:14:02', 1, '暮光：城中城', '2019-03-09 15:43:58', 0);
+INSERT INTO `wiki_page` VALUES (3, 1, '升级日志', 0, 0, 0, 0, 1, '暮光：城中城', '2019-03-09 14:16:20', 1, '暮光：城中城', '2019-03-09 15:46:05', 0);
+INSERT INTO `wiki_page` VALUES (4, 1, '贡献人员列表', 0, 0, 0, 0, 1, '暮光：城中城', '2019-03-09 15:16:15', 1, '暮光：城中城', '2019-03-09 15:17:12', 0);
+INSERT INTO `wiki_page` VALUES (5, 1, 'zyplayer-doc-swagger', 0, 0, 0, 0, 1, '暮光：城中城', '2019-03-09 15:33:14', NULL, NULL, NULL, 0);
+INSERT INTO `wiki_page` VALUES (6, 1, '如何使用', 5, 0, 0, 0, 1, '暮光：城中城', '2019-03-09 15:33:33', 1, '暮光：城中城', '2019-03-09 15:33:46', 0);
 
 -- ----------------------------
 -- Table structure for wiki_page_comment
@@ -145,17 +143,7 @@ CREATE TABLE `wiki_page_comment`  (
   `create_time` datetime(0) NULL DEFAULT NULL COMMENT '创建时间',
   `del_flag` tinyint(4) NULL DEFAULT 0 COMMENT '0=有效 1=删除',
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 17 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
-
--- ----------------------------
--- Records of wiki_page_comment
--- ----------------------------
-INSERT INTO `wiki_page_comment` VALUES (11, 14, NULL, '阿斯顿撒多撒多撒多撒大所多', 0, 1, '暮光：城中城', '2019-03-07 10:47:05', 0);
-INSERT INTO `wiki_page_comment` VALUES (12, 14, NULL, '阿斯顿撒多撒多撒多撒大所多阿斯顿撒多撒多撒多撒大所多阿斯顿撒多撒多撒多撒大所多', 0, 1, '暮光：城中城', '2019-03-07 12:53:07', 0);
-INSERT INTO `wiki_page_comment` VALUES (13, 14, 12, '大声道撒大所大所', 0, 1, '暮光：城中城', '2019-03-07 13:01:49', 0);
-INSERT INTO `wiki_page_comment` VALUES (14, 14, NULL, '大声道撒大所大所\n大声道撒大所大所\n大声道撒大所大所', 0, 1, '暮光：城中城', '2019-03-07 13:11:02', 0);
-INSERT INTO `wiki_page_comment` VALUES (15, 14, 12, '大声道撒大所大所\n大声道撒大所大所\n大声道撒大所大所', 0, 1, '暮光：城中城', '2019-03-07 13:11:12', 0);
-INSERT INTO `wiki_page_comment` VALUES (16, 14, 12, '阿斯顿撒多撒多撒多撒大所多阿斯顿撒多撒多撒多撒大所多阿斯顿撒多撒多撒多撒大所多阿斯顿撒多撒多撒多撒大所多阿斯顿撒多撒多撒多撒大所多阿斯顿撒多撒多撒多撒大所多阿斯顿撒多撒多撒多撒大所多阿斯顿撒多撒多撒多撒大所多阿斯顿撒多撒多撒多撒大所多', 0, 1, '暮光：城中城', '2019-03-07 13:11:27', 0);
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Table structure for wiki_page_content
@@ -173,23 +161,17 @@ CREATE TABLE `wiki_page_content`  (
   `update_time` datetime(0) NULL DEFAULT NULL COMMENT '修改时间',
   PRIMARY KEY (`id`) USING BTREE,
   UNIQUE INDEX `uk_page_id`(`page_id`) USING BTREE COMMENT '页面ID'
-) ENGINE = InnoDB AUTO_INCREMENT = 13 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 7 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of wiki_page_content
 -- ----------------------------
-INSERT INTO `wiki_page_content` VALUES (1, 7, '<p>测试1测试1测试1测试1测试1</p>', 1, NULL, '2019-02-24 17:51:56', 1, NULL, '2019-02-24 17:51:56');
-INSERT INTO `wiki_page_content` VALUES (2, 8, '<p>测试2测试2测试2测试2fasfafafdsafasfxxxxxxxxxxxxdsadsadsadasdasd</p>', 1, NULL, '2019-02-24 17:51:56', 1, '暮光：城中城', '2019-03-08 22:12:36');
-INSERT INTO `wiki_page_content` VALUES (3, 9, '<p>测试3测试3测试3测试3测试3</p>', 1, NULL, '2019-02-24 17:51:56', 1, '暮光：城中城', '2019-03-08 22:12:51');
-INSERT INTO `wiki_page_content` VALUES (4, 10, '<p>测试4测试4测试4测试4测试4</p>', 1, NULL, '2019-02-24 17:51:56', 1, NULL, '2019-02-24 17:51:56');
-INSERT INTO `wiki_page_content` VALUES (5, 11, '<p>测试5测试5测试5测试5</p>', 1, NULL, '2019-02-24 17:51:56', 1, NULL, '2019-02-24 17:51:56');
-INSERT INTO `wiki_page_content` VALUES (6, 12, '<p>测试6测试6测试6测试6测试6</p>', 1, NULL, '2019-02-24 17:51:56', 1, NULL, '2019-02-24 17:51:56');
-INSERT INTO `wiki_page_content` VALUES (7, 13, '<p>测试7测试7测试7测试7</p>', 1, NULL, '2019-02-24 17:51:56', NULL, NULL, '2019-02-24 17:51:56');
-INSERT INTO `wiki_page_content` VALUES (8, 14, '<p>测试7 测试7测试7测试7&nbsp;</p>', 1, NULL, '2019-02-24 17:51:56', 1, NULL, '2019-02-24 17:51:56');
-INSERT INTO `wiki_page_content` VALUES (9, 15, '<p>啦啦啦啦，我是数据库团队介绍</p><p>啦啦啦啦，我是数据库团队介绍&nbsp;</p><p>啦啦啦啦，我是数据库团队介绍</p><p>啦啦啦啦，我是数据库团队介绍</p><p>啦啦啦啦，我是数据库团队介绍</p><p>啦啦啦啦，我是数据库团队介绍</p><p>啦啦啦啦，我是数据库团队介绍&nbsp;<br></p>', 1, NULL, '2019-03-07 22:00:13', NULL, NULL, NULL);
-INSERT INTO `wiki_page_content` VALUES (10, 16, '<p>啦啦啦啦，我是数据库团队介绍&nbsp;&nbsp;<br></p><p>啦啦啦啦，我是数据库团队介绍&nbsp;&nbsp;&nbsp;</p><p>啦啦啦啦，我是数据库团队介绍&nbsp;&nbsp;</p><p>啦啦啦啦，我是数据库团队介绍&nbsp;&nbsp;</p><p>啦啦啦啦，我是数据库团队介绍&nbsp; &nbsp;<br></p>', 1, '暮光：城中城', '2019-03-07 22:05:34', NULL, NULL, NULL);
-INSERT INTO `wiki_page_content` VALUES (11, 17, '<p>啦啦啦啦，我是数据库团队介绍&nbsp;</p><p>啦啦啦啦，我是数据库团队介绍</p><p>啦啦啦啦，我是数据库团队介绍</p><p>啦啦啦啦，我是数据库团队介绍</p><p>啦啦啦啦，我是数据库团队介绍&nbsp;<br></p>', 1, '暮光：城中城', '2019-03-07 22:07:10', NULL, NULL, NULL);
-INSERT INTO `wiki_page_content` VALUES (12, 18, '<p>我的人员列表&nbsp;</p><p>我的人员列表</p><p>我的人员列表</p><p>我的人员列表&nbsp;<br></p>', 1, '暮光：城中城', '2019-03-07 22:28:47', NULL, NULL, NULL);
+INSERT INTO `wiki_page_content` VALUES (1, 1, '<p>一入开源深似海</p><br>源码地址：<a href=\"https://gitee.com/zyplayer/zyplayer-doc\" target=\"_blank\">https://gitee.com/zyplayer/zyplayer-doc</a><p>体验地址：<a href=\"http://doc.zyplayer.com/zyplayer-doc-manage/static/manage/home.html\" target=\"_blank\">http://doc.zyplayer.com/zyplayer-doc-manage/static/manage/home.html</a>&nbsp;账号：zyplayer 密码：123456</p><p><br></p><p>更多实用功能请提交评论或加群交流！谢谢！<br></p><p>QQ群：466363173</p>tips：想要编辑本文请修改表：wiki_page的edit_type字段值为0才行哦~<p><br></p><p>有什么问题可以来评论本文章哦！当然，如果你是部署在你们自己本地的我肯定看不到此文章的评论</p><p>可以到本工具官网下评论此文章，我会随时去查看的！</p><p>唯一官网地址：<a href=\"http://doc.zyplayer.com\" target=\"_blank\">http://doc.zyplayer.com</a></p><p><br></p><p>欢迎提交各种变态无理的要求~</p>', 1, '暮光：城中城', '2019-03-09 14:01:41', 1, '暮光：城中城', '2019-03-09 15:30:22');
+INSERT INTO `wiki_page_content` VALUES (2, 2, '<p>zyplayer-doc-manage：</p><p>1、首页做大的调整优化，希望使用element重构一下</p><p>2、人员导入方案，权限优化，人员管理细化</p><p>&nbsp;</p><p>zyplayer-doc-wiki：</p><p><prefira code\';font-size:10.5pt;\"=\"\">1<spancourier new\';\"=\"\">、支持页面权限控制，包括查看、编辑、删除、评论的权限</spancourier></prefira></p><p><prefira code\';font-size:10.5pt;\"=\"\"><spancourier new\';\"=\"\"><spancourier new\';\"=\"\">2<spancourier new\';\"=\"\">、</spancourier></spancourier></spancourier></prefira>支持把一个空间里的文档一键同步到git<spancourier new\';\"=\"\">的wiki上，统一管理文档</spancourier></p><p><spancourier new\';\"=\"\">3、</spancourier>支持微信文章拉取保存</p><p>4、支持历史记录查看</p><p>5、支持提供多种编辑器选择，更大的选择空间</p><p>6、支持全局搜索、文章内搜索等，当前只支持标题搜索，尴尬</p><p>7、支持开放一个空间里的文档无需登录即可访问</p><p>8、url动态变动，这样就可以复制指定文章的地址了</p><p>9、参考https://www.kancloud.cn，取其精华</p><p><br></p><p>欢迎加入开发！</p><p>如果您觉得哪里不好用也可以拉取源码下来改了后提交PR</p><p>源码地址：<a href=\"https://gitee.com/zyplayer/zyplayer-doc\" target=\"_blank\">https://gitee.com/zyplayer/zyplayer-doc</a></p><p><br></p><p>更多实用功能请提交评论或加群交流！谢谢！</p><p>QQ群：466363173</p>', 1, '暮光：城中城', '2019-03-09 14:14:02', 1, '暮光：城中城', '2019-03-09 15:43:58');
+INSERT INTO `wiki_page_content` VALUES (3, 3, '<p><span style=\"color: rgb(249, 150, 59);\">V1.0.1　2019-03-09</span></p><p>1、增加zyplayer-doc-wiki模块</p><ul><li>支持空间隔离<br></li><li>支持附件上传<br></li><li>拖动可以改变wiki顺序<br></li><li>基本的文档创建、编辑、评论、删除、搜索等功能</li></ul><p><prefira code\';font-size:10.5pt;\"=\"\">2<spancourier new\';\"=\"\">、dubbo<spancourier new\';\"=\"\">文档支持使用元数据生成参数和返回值，dubbo2.7.0新特性</spancourier></spancourier></prefira></p><p><prefira code\';font-size:10.5pt;\"=\"\"><spancourier new\';\"=\"\"><spancourier new\';\"=\"\">3<spancourier new\';\"=\"\">、框架进行了大的拆分，表修改较多</spancourier></spancourier></spancourier></prefira></p><p><prefira code\';font-size:10.5pt;\"=\"\"><spancourier new\';\"=\"\"><spancourier new\';\"=\"\"><spancourier new\';\"=\"\">4<spancourier new\';\"=\"\">、增加升级通知</spancourier></spancourier></spancourier></spancourier></prefira></p><p><prefira code\';font-size:10.5pt;\"=\"\"><spancourier new\';\"=\"\"><spancourier new\';\"=\"\"><spancourier new\';\"=\"\"><spancourier new\';\"=\"\">5<spancourier new\';\"=\"\">、细节优化</spancourier></spancourier></spancourier></spancourier></spancourier></prefira></p><p><prefira code\';font-size:10.5pt;\"=\"\"><spancourier new\';\"=\"\"><spancourier new\';\"=\"\"><spancourier new\';\"=\"\"><spancourier new\';\"=\"\"><spancourier new\';\"=\"\"><br></spancourier></spancourier></spancourier></spancourier></spancourier></prefira></p><p><span style=\"color: rgb(249, 150, 59);\">V1.0.0　2019-02-15</span><br></p><p>第一个版本发布</p><p>1、zyplayer-doc-swagger，swagger接口文档展示方案，在之前的项目中有许多改进，坚持文档生成和展示分离，有较多需要服务端支持的功能，不对各项目做过多的入侵即可使用，可动态对文档进行开放访问<br>2、zyplayer-doc-dubbo，支持对dubbo服务的自动扫描，直观展示所有的服务，文档的展示、文档编辑和在线接口调试，不需要对已有的服务做任何改动，支持zookeeper、nacos注册中心的服务扫描<br>3、zyplayer-doc-db，数据库文档工具，具有数据库表、字段文档的查看/修改，文档导出等功能，支持mysql和sqlserver数据库<br>4、zyplayer-doc-manage，管理以上几个服务，spring boot项目，spring security做权限管理，可直接运行</p>', 1, '暮光：城中城', '2019-03-09 14:16:20', 1, '暮光：城中城', '2019-03-09 15:46:05');
+INSERT INTO `wiki_page_content` VALUES (4, 4, '<p>开发人员列表：</p><p><a href=\"http://www.zyplayer.com\" target=\"_blank\">暮光：城中城</a><br></p><p><br></p><p>孤零零的一人，，（希望能早点删除本行）</p>', 1, '暮光：城中城', '2019-03-09 15:16:15', 1, '暮光：城中城', '2019-03-09 15:17:12');
+INSERT INTO `wiki_page_content` VALUES (5, 5, '<p>zyplayer-doc-swagger 使用文档目录</p>', 1, '暮光：城中城', '2019-03-09 15:33:14', NULL, NULL, NULL);
+INSERT INTO `wiki_page_content` VALUES (6, 6, '<p>最不喜欢写文档了。。。</p>', 1, '暮光：城中城', '2019-03-09 15:33:33', 1, '暮光：城中城', '2019-03-09 15:33:46');
 
 -- ----------------------------
 -- Table structure for wiki_page_file
@@ -210,12 +192,7 @@ CREATE TABLE `wiki_page_file`  (
   `del_flag` tinyint(4) NULL DEFAULT 0 COMMENT '0=有效 1=删除',
   PRIMARY KEY (`id`) USING BTREE,
   UNIQUE INDEX `uk_uuid`(`uuid`) USING BTREE COMMENT '文件ID'
-) ENGINE = InnoDB AUTO_INCREMENT = 23 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
-
--- ----------------------------
--- Records of wiki_page_file
--- ----------------------------
-INSERT INTO `wiki_page_file` VALUES (22, 17, 'loading_mask.png', 'e:/tmp/wikiFiles/2019/03/07/aac61c98a8464eefb38969e3e5e432ee.png', 'aac61c98a8464eefb38969e3e5e432ee', 1, '暮光：城中城', '2019-03-07 22:21:08', NULL, NULL, NULL, 0);
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Table structure for wiki_page_zan
@@ -230,15 +207,12 @@ CREATE TABLE `wiki_page_zan`  (
   `create_time` datetime(0) NULL DEFAULT NULL COMMENT '创建时间',
   `yn` tinyint(4) NULL DEFAULT NULL COMMENT '是否有效 0=无效 1=有效',
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 11 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 2 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of wiki_page_zan
 -- ----------------------------
-INSERT INTO `wiki_page_zan` VALUES (7, 8, NULL, 1, '暮光：城中城', '2019-03-06 19:04:26', 1);
-INSERT INTO `wiki_page_zan` VALUES (8, 14, NULL, 1, '暮光：城中城', '2019-03-07 10:32:18', 1);
-INSERT INTO `wiki_page_zan` VALUES (9, 17, NULL, 1, '暮光：城中城', '2019-03-07 22:10:56', 0);
-INSERT INTO `wiki_page_zan` VALUES (10, 18, NULL, 1, '暮光：城中城', '2019-03-07 22:53:23', 1);
+INSERT INTO `wiki_page_zan` VALUES (1, 1, NULL, 1, '暮光：城中城', '2019-03-09 22:19:44', 0);
 
 -- ----------------------------
 -- Table structure for wiki_space
@@ -249,21 +223,19 @@ CREATE TABLE `wiki_space`  (
   `name` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '空间名',
   `type` tinyint(4) NULL DEFAULT 1 COMMENT '空间类型 1=公司 2=个人 3=私人',
   `space_explain` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '描述',
+  `edit_type` tinyint(4) NOT NULL DEFAULT 0 COMMENT '编辑类型 0=可编辑 1=不允许编辑',
   `create_user_id` bigint(20) NULL DEFAULT NULL COMMENT '创建人ID',
   `create_user_name` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '创建人名字',
   `create_time` datetime(0) NULL DEFAULT NULL COMMENT '创建时间',
   `del_flag` tinyint(4) NOT NULL DEFAULT 0 COMMENT '删除标记 0=正常 1=已删除',
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 6 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 3 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of wiki_space
 -- ----------------------------
-INSERT INTO `wiki_space` VALUES (1, '研发中心', 1, 'xxxxx', 1, NULL, '2019-02-24 17:51:56', 0);
-INSERT INTO `wiki_space` VALUES (2, '数据库团队', 1, '数据库、redis、kafka相关', 1, '暮光：城中城', '2019-03-07 21:49:49', 0);
-INSERT INTO `wiki_space` VALUES (3, '测试空间', 1, '', 1, '暮光：城中城', '2019-03-07 21:53:51', 1);
-INSERT INTO `wiki_space` VALUES (4, '测试2', 1, '', 1, '暮光：城中城', '2019-03-07 21:54:25', 1);
-INSERT INTO `wiki_space` VALUES (5, '研发中心', 1, 'dasdasdasdasd', 1, '暮光：城中城', '2019-03-08 22:27:30', 1);
+INSERT INTO `wiki_space` VALUES (1, 'zyplayer-doc交流专用', 1, '', 1, 1, '暮光：城中城', '2019-03-09 13:59:14', 0);
+INSERT INTO `wiki_space` VALUES (2, '体验专用空间', 1, '', 1, 1, '暮光：城中城', '2019-03-09 14:24:30', 0);
 
 -- ----------------------------
 -- Table structure for zyplayer_storage
@@ -282,5 +254,4 @@ CREATE TABLE `zyplayer_storage`  (
 -- ----------------------------
 -- Records of zyplayer_storage
 -- ----------------------------
-
 SET FOREIGN_KEY_CHECKS = 1;
