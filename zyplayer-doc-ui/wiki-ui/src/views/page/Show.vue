cec0e2d5c8d6d918ceea4344b146bab7c74c6525
暮光：城中城 <806783409@qq.com>
2020-10-11 21:45:17 +0800

    #I1XRJM wiki文档样式处理

--- zyplayer-doc-ui/wiki-ui/src/views/page/Show.vue
+++ zyplayer-doc-ui/wiki-ui/src/views/page/Show.vue
@@ -645,5 +645,29 @@
 	.page-show-vue .comment-card:hover .comment-user-name .el-icon-delete{
 		display: inline-block;
 	}
+	/**展示内容的样式*/
+	.wiki-content table {
+		border-top: 1px solid #ccc; border-left: 1px solid #ccc;
+	}
+	.wiki-content table td, .wiki-content table th {
+		border-bottom: 1px solid #ccc; border-right: 1px solid #ccc; padding: 3px 5px;
+	}
+	.wiki-content table th {
+		border-bottom: 2px solid #ccc; text-align: center;
+	}
+	.wiki-content blockquote {
+		display: block; border-left: 8px solid #d0e5f2; padding: 5px 10px; margin: 10px 0;
+		line-height: 1.4; font-size: 100%; background-color: #f1f1f1;
+	}
+	.wiki-content code {
+		display: inline-block; *display: inline; *zoom: 1;
+		background-color: #f1f1f1; border-radius: 3px; padding: 3px 5px; margin: 0 3px;
+	}
+	.wiki-content pre code {
+		display: block;
+	}
+	.wiki-content ul, ol {
+		margin: 10px 0 10px 20px;
+	}
 </style>
 
