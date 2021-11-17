ce84b4fee9acf681788379645f0a4d22f2e83086
暮光：城中城 <806783409@qq.com>
2021-11-18 00:03:07 +0800

    参数名设置

--- zyplayer-doc-data/src/main/java/com/zyplayer/doc/data/repository/manage/mapper/UserGroupAuthMapper.java
+++ zyplayer-doc-data/src/main/java/com/zyplayer/doc/data/repository/manage/mapper/UserGroupAuthMapper.java
@@ -2,6 +2,7 @@ package com.zyplayer.doc.data.repository.manage.mapper;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.zyplayer.doc.data.repository.manage.entity.UserGroupAuth;
+import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Select;
 
 /**
@@ -16,5 +17,5 @@ public interface UserGroupAuthMapper extends BaseMapper<UserGroupAuth> {
 	
 	@Select("select a.id from user_group_auth a join user_group_relation b on a.group_id=b.group_id and b.user_id=#{userId} " +
 			"where a.project_type=#{projectType} and a.auth_type=#{authType} and a.data_id=#{spaceId} and b.del_flag=0")
-	Long haveAuth(Long spaceId, Integer projectType, Integer authType, Long userId);
+	Long haveAuth(@Param("spaceId") Long spaceId, @Param("projectType") Integer projectType, @Param("authType") Integer authType, @Param("userId") Long userId);
 }
