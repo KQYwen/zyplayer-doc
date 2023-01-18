e4920791b7fd04b3c7e33869194be903ec801330
diantu <diantu188@163.com>
2023-01-18 11:40:47 +0800

    sqlserver兼容性优化

--- zyplayer-doc-db/src/main/java/com/zyplayer/doc/db/service/database/OracleServiceImpl.java
+++ zyplayer-doc-db/src/main/java/com/zyplayer/doc/db/service/database/OracleServiceImpl.java
@@ -48,8 +48,8 @@ public class OracleServiceImpl extends DbBaseService {
         }
         StringBuilder sqlSbFinal = new StringBuilder();
         Integer pageSize = dataViewParam.getPageSize() * dataViewParam.getPageNum();
-        Integer offsetSize = dataViewParam.getPageSize() * (dataViewParam.getPageNum() - 1) + 1;
-        sqlSbFinal.append(String.format("select * from ( select %s from %s", queryColumns + ",rownum rn", "(" + sqlSb + ") where rownum<=" + pageSize + " ) t2 where t2.rn >=" + offsetSize));
+        Integer pageNum = dataViewParam.getPageSize() * (dataViewParam.getPageNum() - 1) + 1;
+        sqlSbFinal.append(String.format("select * from ( select %s from %s", queryColumns + ",rownum rn", "(" + sqlSb + ") where rownum<=" + pageSize + " ) t2 where t2.rn >=" + pageNum));
         return sqlSbFinal.toString();
     }
 }
