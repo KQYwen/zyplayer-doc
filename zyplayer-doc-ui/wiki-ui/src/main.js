51c8b8949e474eedd1fefb8a071894e97b976061
sswiki <admin@zyplayer.com>
2024-12-04 22:48:10 +0800

    #IAQW9I 解决开放文档列表展示样式问题

--- zyplayer-doc-ui/wiki-ui/src/main.js
+++ zyplayer-doc-ui/wiki-ui/src/main.js
@@ -8,13 +8,13 @@ import App from './App.vue'
 import {createRouter, createWebHashHistory} from 'vue-router'
 
 import ElementUI from 'element-plus'
-import 'element-plus/dist/index.css'
-import './assets/scss/base.scss'
 import Antd from 'ant-design-vue';
 import zhCn from 'element-plus/es/locale/lang/zh-cn'
 import routes from './routes'
 import Vant from 'vant'
 import {createPinia} from 'pinia'
+import 'element-plus/dist/index.css'
+import './assets/scss/base.scss'
 
 const router = createRouter({
 	history: createWebHashHistory(),
